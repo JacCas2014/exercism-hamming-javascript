62bc3e029b591c3100903cacdcfbfc3ab1d644bc
Jac Casey <JacCas2014@users.noreply.github.com>
2016-03-24 23:30:38 +0000

    Jac - comparing to equal dna strings

--- hamming.js
+++ hamming.js
@@ -2,8 +2,10 @@ function Hamming() {
 
 }
 
-Hamming.prototype.compute = function() {
-
+Hamming.prototype.compute = function(dnaA, dnaB) {
+    if(dnaA === dnaB) {
+        return 0;
+    }
 }
 
 module.exports = Hamming;
