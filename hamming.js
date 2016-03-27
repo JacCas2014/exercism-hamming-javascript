4d50510f19ad32d0bc07d0b94dba3f88cc6bf527
Jac Casey <JacCas2014@users.noreply.github.com>
2016-03-27 22:42:26 +0100

    Jac - throw error if the strings are not equal in length

--- hamming.js
+++ hamming.js
@@ -3,6 +3,10 @@ function Hamming() {
 }
 
 Hamming.prototype.compute = function(dnaA, dnaB) {
+    if (dnaA.length != dnaB.length) {
+        throw new Error('DNA strands must be of equal length.');
+    }
+
     var hammingDistance = 0;
     for (var i = 0; i < dnaA.length; i++) {
         if (dnaA[i] != dnaB[i]) {
